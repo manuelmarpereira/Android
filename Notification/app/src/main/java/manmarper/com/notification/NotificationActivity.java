c129659f64c7ed94cf82582ac9002ad0fd46a336
manuelmarpereira <manelmarper@gmail.com>
2016-02-18 16:56:02 +0000

    simple notification

--- Notification/app/src/main/java/manmarper/com/notification/NotificationActivity.java
+++ Notification/app/src/main/java/manmarper/com/notification/NotificationActivity.java
@@ -1,7 +1,12 @@
 package manmarper.com.notification;
 
+import android.app.NotificationManager;
+import android.content.Context;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.NotificationCompat;
+import android.view.View;
+import android.widget.Button;
 
 public class NotificationActivity extends AppCompatActivity {
 
@@ -9,5 +14,25 @@ public class NotificationActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_notification);
+
+        //Initialize Buttons
+        Button btnShow = (Button) findViewById(R.id.btn_show);
+        
+        //Set listener on notification button
+        btnShow.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                NotificationCompat.Builder mBuilder =
+                        (NotificationCompat.Builder) new NotificationCompat.Builder(NotificationActivity.this)
+                                .setSmallIcon(android.R.drawable.stat_sys_download_done)
+                                .setContentTitle("My notification")
+                                .setContentText("Hello World!");
+
+                NotificationManager mNotificationManager = (NotificationManager) getSystemService(Context.NOTIFICATION_SERVICE);
+                mNotificationManager.notify(0, mBuilder.build());
+            }
+
+        });
+
     }
 }
